c39fa530fdaf58e89916a251e17383ecc6eae3f8
지인 <sonia9.shin@gmail.com>
2021-07-14 05:59:49 +0900

    Create app4.js
    익스프레스 요청에 객체로 응답하기

---
+++ 05_웹서버 만들기/ExpressExample/app4.js
@@ -0,0 +1,16 @@
+//Express 기본 모듈 불러오기
+var express = require('express')
+    , http = require('http');
+
+//익스프레스 객체 생성
+var app = express();
+
+app.use(function(req, res, next){
+    console.log('첫 번째 미들웨어에서 요청을 처리함.');
+    
+    res.send({name:'소녀시대', age:20});
+});
+
+http.createServer(app).listen(3000, function(){
+    console.log('Express 서버가 3000번 포트에서 시작됨.');
+});
